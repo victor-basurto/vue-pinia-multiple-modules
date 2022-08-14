69b71bcd31dc70fc49e79b87367556897869c182
Viktor Basurto <my.mvplace@gmail.com>
2022-08-14 13:56:58 -0700

    fixed typo in `apiStatusType`

--- src/@types/interfaces/response.interface.ts
+++ src/@types/interfaces/response.interface.ts
@@ -1,7 +1,7 @@
 import type { IStrain } from '@/@types/interfaces/strain.interface';
 import type { ApiStatusType } from '../api';
 
-export const apiStatausType: ApiStatusType = {
+export const apiStatusType: ApiStatusType = {
 	IDLE: 'IDLE',
 	LOADING: 'LOADING',
 	SUCCESS: 'SUCCESS',
